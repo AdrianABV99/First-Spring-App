ba20f32f62f30a37ccae221ea5f84073484d621b
Adrian <adrian99achim@gmail.com>
2021-07-14 20:33:00 +0300

    Student Class

--- src/main/java/com/fsa/fsa/FsaApplication.java
+++ src/main/java/com/fsa/fsa/FsaApplication.java
@@ -1,10 +1,12 @@
 package com.fsa.fsa;
-
+import com.fsa.fsa.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,7 +18,13 @@ public class FsaApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello","World");
+	public List<Student> hello() {
+		return List.of(new Student(
+				1L,
+				"Adrian",
+				"adrian.achim@email.com",
+				LocalDate.of(1999, Month.OCTOBER,6),
+				21
+		));
 	}
 }
