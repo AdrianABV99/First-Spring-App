97d82dca7955cec1336455ae0d6a9980d6f019a9
Adrian <adrian99achim@gmail.com>
2021-07-14 21:14:24 +0300

    depedency injection

--- src/main/java/com/fsa/fsa/Student/StudentService.java
+++ src/main/java/com/fsa/fsa/Student/StudentService.java
@@ -1,9 +1,13 @@
 package com.fsa.fsa.Student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
     public List<Student> getStudents() {
         return List.of(new Student(
