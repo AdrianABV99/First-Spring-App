0c9a67218c793b11c728651400fb401318fdada0
Adrian <adrian99achim@gmail.com>
2021-07-15 01:43:51 +0300

    Saving Students

---
+++ src/main/java/com/fsa/fsa/Student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.fsa.fsa.Student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args-> {
+          Student adi = new Student(
+
+                    "Adrian",
+                    "adrian.achim@email.com",
+                    LocalDate.of(1999, Month.OCTOBER, 6),
+                    21
+                );
+          Student ale = new Student(
+
+                  "ale",
+                  "ale.alexandra@email.com",
+                  LocalDate.of(2000, Month.MAY, 29),
+                  21
+          );
+
+          repository.saveAll(List.of(adi,ale));
+            };
+        }
+
+}
